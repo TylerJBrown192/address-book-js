ef3670d846108b87dbd1e40b653d7af0acd24d3d
David Zulaica and Tyler Brown <elementalplug@gmail.com>
2015-02-17 11:05:45 -0800

    Update javascript and adds id to select class

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,44 @@
 $(document).ready(function() {
-  $("form#new-contact").submit(function(event) {
+  $("#new-contact").submit(function(event) {
     event.preventDefault();
 
-    var inputtedFirstName = $("input#new-first-name").val();
-    var inputtedLastName = $("input#new-last-name").val();
-    var inputtedAddress = $("input#new-address").val();
-    var newContact = { firstName: inputtedFirstName, lastName: inputtedLastName, address: inputtedAddress };
+    var inputtedFirstName = $("#new-first-name").val();
+    var inputtedLastName  = $("#new-last-name").val();
+
+    var inputtedStreet    = $("#new-street").val();
+    var inputtedCity      = $("#new-city").val();
+    var inputtedState     = $("#new-state").val();
+    var inputtedZip       = $("#new-zip").val();
+
+    var newAddress = { street: inputtedStreet,
+                       city: inputtedCity,
+                       state: inputtedState,
+                       zip: inputtedZip,
+                        fullAddress: function() {
+                         return this.street +
+                         ", " +
+                         this.city +
+                         ", " +
+                         this.state +
+                         " " +
+                         this.zip;
+                       }
+                     };
+
+    var newContact = { firstName: inputtedFirstName,
+                       lastName: inputtedLastName,
+                       address: newAddress.fullAddress()
+                      };
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.firstName + " " + newContact.lastName + "</span></li>")
 
 
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
-    $("input#new-address").val("");
+    $("#new-first-name").val("");
+    $("#new-last-name").val("");
+    $("#new-street").val("");
+    $("#new-city").val("");
+    $("#new-state").val("");
+    $("#new-zip").val("");
 
     $(".contact").last().click(function() {
       $("#show-contact").show();
